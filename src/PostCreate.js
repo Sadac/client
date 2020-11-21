81a3e86cbcf6fab4be17c95d9250a46626714792
Rocco <sadarocco@gmail.com>
2020-11-21 09:40:27 -0300

    first commit push to master

---
+++ src/PostCreate.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+export default () => {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async (event) => {
+    try {
+      event.preventDefault();
+
+      await axios.post('http://localhost:4000/posts', {
+        title
+      });
+
+      setTitle('');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return <div>
+    <form onSubmit={onSubmit}>
+      <div>
+        <label className='form-group'>Title</label>
+        <input
+          value={title}
+          onChange={ e => setTitle( e.target.value )}
+          className='form-control'
+        />
+      </div>
+      <button className='btn btn-primary'>Submit</button>
+    </form>
+  </div>
+}
